857c488235506eff4d5763dc62aeae2cb3e7c7bc
madhuraa11093 <madhura.ansingkar@gmail.com>
2018-04-03 12:22:20 -0700

    updated signup

--- main.js
+++ main.js
@@ -1,124 +1,147 @@
 $(document).ready(function() {
   $('#contact_form').bootstrapValidator({
-      // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
-      feedbackIcons: {
-        valid: 'glyphicon glyphicon-ok',
-        invalid: 'glyphicon glyphicon-remove',
-        validating: 'glyphicon glyphicon-refresh'
+    // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+    fields: {
+      first_name: {
+        validators: {
+          stringLength: {
+            min: 2,
+          },
+          notEmpty: {
+            message: 'Please enter your First Name'
+          }
+        }
       },
-      fields: {
-        first_name: {
-          validators: {
-            stringLength: {
-              min: 2,
-            },
-            notEmpty: {
-              message: 'Please enter your First Name'
-            }
+      last_name: {
+        validators: {
+          stringLength: {
+            min: 2,
+          },
+          notEmpty: {
+            message: 'Please enter your Last Name'
           }
-        },
-        last_name: {
-          validators: {
-            stringLength: {
-              min: 2,
-            },
-            notEmpty: {
-              message: 'Please enter your Last Name'
-            }
+        }
+      },
+      username: {
+        validators: {
+          stringLength: {
+            min: 8,
+          },
+          notEmpty: {
+            message: 'Please enter your Username'
           }
-        },
-        user_name: {
-          validators: {
-            stringLength: {
-              min: 8,
-            },
-            notEmpty: {
-              message: 'Please enter your Username'
-            }
+        }
+      },
+      password: {
+        validators: {
+          stringLength: {
+            min: 8,
+          },
+          notEmpty: {
+            message: 'Please enter your Password'
           }
-        },
-        user_password: {
-          validators: {
-            stringLength: {
-              min: 8,
-            },
-            notEmpty: {
-              message: 'Please enter your Password'
-            }
+        }
+      },
+      email: {
+        validators: {
+          notEmpty: {
+            message: 'Please enter your Email Address'
+          },
+          emailAddress: {
+            message: 'Please enter a valid Email Address'
           }
-        },
-        confirm_password: {
-          validators: {
-            stringLength: {
-              min: 8,
-            },
-            notEmpty: {
-              message: 'Please confirm your Password'
-            }
+        }
+      },
+      contact_no: {
+        validators: {
+          stringLength: {
+            min: 10,
+            max: 10,
+          },
+          notEmpty: {
+            message: 'Please enter your Contact No.'
           }
-        },
-        email: {
-          validators: {
-            notEmpty: {
-              message: 'Please enter your Email Address'
-            },
-            emailAddress: {
-              message: 'Please enter a valid Email Address'
-            }
-          }
-        },
-        contact_no: {
-          validators: {
-            stringLength: {
-              min: 10,
-              max: 10,
-            },
-            notEmpty: {
-              message: 'Please enter your Contact No.'
-            }
 
+        }
+      },
+      department: {
+        validators: {
+          notEmpty: {
+            message: 'Please choose your Profession'
           }
-        },
-        department: {
-          validators: {
-            notEmpty: {
-              message: 'Please choose your Profession'
-            }
+        }
+      },
+      about_me: {
+        validators: {
+          stringLength: {
+            min: 10,
+            max: 1000,
+          },
+          notEmpty: {
+            message: 'Please provide your details'
           }
-        },
-        about_me: {
-          validators: {
-            stringLength: {
-              min: 10,
-              max: 1000,
-            },
-            notEmpty: {
-              message: 'Please provide your details'
-            }
 
+        }
+      },
+      image: {
+        validators: {
+          notEmpty: {
+            message: 'Please provide your image'
           }
-        },
+        }
+      },
+    }
+  })
+});
 
+function ValidateSize(file) {
+  var FileSize = file.files[0].size / 1024 / 1024; // in MB
+  if (FileSize > 2) {
+    alert('File size exceeds 2 MB');
+    $(file).val(''); //for clearing with Jquery
+  } else {
+
+  }
+}
+$(document).ready(function() {
+  $(document).on('change', '.btn-file :file', function() {
+    var input = $(this),
+      label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+    input.trigger('fileselect', [label]);
+  });
+
+  $('.btn-file :file').on('fileselect', function(event, label) {
+
+    var input = $(this).parents('.input-group').find(':jpg'),
+      log = label;
+
+    if (input.length) {
+      input.val(log);
+    } else {
+      if (log) alert(log);
+    }
+
+
+  });
+
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function(e) {
+        $('#img-upload').attr('src', e.target.result);
       }
-    })
-    .on('success.form.bv', function(e) {
-      $('#success_message').slideDown({
-        opacity: "show"
-      }, "slow") // Do something ...
-      $('#contact_form').data('bootstrapValidator').resetForm();
 
-      // Prevent form submission
-      e.preventDefault();
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
 
-      // Get the form instance
-      var $form = $(e.target);
-
-      // Get the BootstrapValidator instance
-      var bv = $form.data('bootstrapValidator');
-
-      // Use Ajax to submit form data
-      $.post($form.attr('action'), $form.serialize(), function(result) {
-        console.log(result);
-      }, 'json');
-    });
+  $("#imgInp").change(function() {
+    readURL(this);
+  });
 });
